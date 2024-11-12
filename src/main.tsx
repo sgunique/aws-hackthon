aa6dbfe71bcc0903a5cae376571df05d3832a377
sgunique <sanugudikandula@gmail.com>
2024-11-12 23:54:04 +0100

    Update the config

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import AuthenticatedApp from "./Authencation";
 import "./index.css";
 import { Amplify } from 'aws-amplify';  // Add this import
-// import config from "../amplify_outputs.json";
+import config from "../amplify_outputs.json";
 
-Amplify.configure({});
+Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
